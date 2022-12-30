2050d73830082ed97d25b6a77e4b92982bd54843
naruto5055 <111577850+naruto5055@users.noreply.github.com>
2022-12-30 07:59:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_3_perfect_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int First, second, X, Y, sum;
+            Console.WriteLine("Enter the first");
+            First = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second");
+            second = int.Parse(Console.ReadLine());
+            for (X = First; X <= second; X++)
+            {
+                Y = 1;
+                sum = 0;
+                while (Y < X)
+                {
+                    if (X % Y == 0)
+                        sum = sum + Y;
+                    Y++;
+                }
+                if (sum == X && X != 0)
+                {
+
+                    Console.WriteLine("{0}", X);
+
+                }
+            }
+        }
+    }
+}
+    
+
